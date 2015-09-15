469777ce381f9dc456b01722051d9ef1932df10d
iman mafi & Vaughn Dice <student@epicodus.com>
2015-09-15 14:43:18 -0700

    add edit account functionality, also stuff

--- js/scripts.js
+++ js/scripts.js
@@ -15,21 +15,22 @@ Account.prototype.deposit = function(amount) {
   this.balance += amount;
 }
 
-// var newCustomer = new Customer('Vin Diesel')
+var newCustomer = new Customer('')
 var newAccount = new Account(0, new Object());
 
 $(document).ready(function() {
+  $("#account-header").text('New Account');
+
   $("form#new-account").submit(function(event) {
-    event.preventDefault();
-    var deposit = parseFloat($("input#initial-deposit").val());
-    var newCustomer = new Customer($("input#customer-name").val());
+    newCustomer.name = $("input#customer-name").val();
     newAccount.customer = newCustomer;
-    newAccount.deposit(deposit);
-    $("#balance").text(newAccount.balance);
-    $(".result").show();
-    $("#hideafterenter").hide();
     $("#customer").text(newCustomer.name);
-    $("#existing").show();
+    updateAccount();
+
+    $("#hideafterenter").hide();
+    $("#transactions").show();
+    $("#edit-account-button").show();
+    $("#account-header").text('Transactions');
   });
 
   $("form#deposit").submit(function(event) {
@@ -40,13 +41,37 @@ $(document).ready(function() {
     updateAccount('withdraw');
   });
 
+  $("#edit-account-button").click(function() {
+    $("#account-header").text('Edit Account');
+    $("#transactions").hide();
+    $("#settings").show();
+  });
+
+  $("form#updated-account").submit(function(event) {
+    newCustomer.name = $("input#updated-customer-name").val();
+    newAccount.customer = newCustomer;
+    $("#customer").text(newCustomer.name);
+    $("#account-header").text('New Account');
+    $("#transactions").show();
+    $("#settings").hide();
+  });
+
 });
 
 function updateAccount(action) {
+  var selector = "input#";
+  var methodToInvoke = action;
+  if (!action) {
+    selector += "initial-deposit";
+    methodToInvoke = "deposit";
+  } else {
+    selector += action;
+  }
+
   event.preventDefault();
-  var value = parseFloat($("input#" + action).val());
-  newAccount[action](value);
+  var value = parseFloat($(selector).val());
+  newAccount[methodToInvoke](value);
   $("#balance").text(newAccount.balance);
   $(".result").show();
-  $("input#" + action).val("");
+  $(selector).val("");
 }
